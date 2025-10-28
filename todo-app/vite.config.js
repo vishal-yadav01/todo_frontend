2ef48aec6c08aa37e2b482722b1af8911d8efd72
unknown <ydev2897@gmail.com>
2025-10-29 01:03:48 +0530

    Fix Render route issue with static.json

--- todo-app/vite.config.js
+++ todo-app/vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     port: 3000,
   },
   build: {
-    sourcemap: false,
+    outDir: 'dist', // default — make sure not changed
   },
 });
